c290d6d889240492f6a1b93b7b1328885a8ef215
allan224 <allanflewis@gmail.com>
2021-11-12 11:58:53 +0530

    test

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MongoDB: 'mongodb+srv://carRental:carRental@carrental.ie0k3.mongodb.net/carRental?retryWrites=true&w=majority',
+    MongoDB: 'mongodb+srv://carRental:carRental@carrental.kerhd.mongodb.net/carRental?retryWrites=true&w=majority',
     FBAppID: '396136942082109',
     FBAppSECRET: 'd9b34f2c7e9af5a6b9c3a4522043388a'
 };
